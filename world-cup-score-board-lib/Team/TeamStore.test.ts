0a40df62c22e3c2d0c4a6fb2387ad3d608143879
Artem Astrouski <ostrovskiy.a.97@gmail.com>
2025-07-06 21:17:41 +0200

    test: implement tests for TeamStore class constructor and methods

--- world-cup-score-board-lib/Team/TeamStore.test.ts
+++ world-cup-score-board-lib/Team/TeamStore.test.ts
@@ -1,13 +1,52 @@
-it('should throw an exception if team name is not a string', () => {});
+import { beforeEach, describe, expect } from '@jest/globals';
+import { TeamStore } from '@/world-cup-score-board-lib/Team/TeamStore';
 
-it('should throw an exception if team name is a whitespace', () => {});
+const TEAM_NAME = 'Mexico';
+
+it('should throw an exception if team name is not a string', () => {
+    // prettier-ignore
+    expect(() => new TeamStore(null as never))
+        .toThrow(`Team name should be a string! Provided: object`);
+
+    // prettier-ignore
+    expect(() => new TeamStore(undefined as never))
+        .toThrow(`Team name should be a string! Provided: undefined`);
+
+    // prettier-ignore
+    expect(() => new TeamStore(100 as never))
+        .toThrow(`Team name should be a string! Provided: number`);
+
+    // prettier-ignore
+    expect(() => new TeamStore(['teamName'] as never))
+        .toThrow(`Team name should be a string! Provided: object`);
+});
+
+it('should throw an exception if team name is a whitespace', () => {
+    expect(() => new TeamStore(' ')).toThrow('Team name can not be empty!');
+    expect(() => new TeamStore('   ')).toThrow('Team name can not be empty!');
+});
 
 describe('setScore', () => {
-    it('should set score', () => {});
+    let team: TeamStore;
 
-    it('should throw an exception if new score less than 0', () => {});
+    beforeEach(() => {
+        team = new TeamStore(TEAM_NAME);
+    });
 
-    it('should throw an exception if score is not a number', () => {});
+    it('should set score', () => {
+        team.setScore(1);
+        expect(team.score).toBe(1);
+    });
 
-    it('should throw an exception if score is higher than 100', () => {});
+    it('should throw an exception if new score less than 0', () => {
+        expect(() => team.setScore(-1)).toThrow('Score can not be less than 0!');
+    });
+
+    it('should throw an exception if score is not a number', () => {
+        expect(() => team.setScore('100' as never)).toThrow('Score should be a number!');
+    });
+
+    it('should throw an exception if score is higher than 100', () => {
+        expect(() => team.setScore(101)).toThrow('Score can not be greater than 100!');
+    });
 });

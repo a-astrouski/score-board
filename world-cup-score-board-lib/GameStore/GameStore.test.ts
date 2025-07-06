50d057659c146f9a09a09a4faf1a3f93caa399f6
Artem Astrouski <ostrovskiy.a.97@gmail.com>
2025-07-06 21:31:18 +0200

    test: implement tests for Game class

--- world-cup-score-board-lib/GameStore/GameStore.test.ts
+++ world-cup-score-board-lib/GameStore/GameStore.test.ts
@@ -1,1 +1,7 @@
-it('should throw an exception if away team and home team are the same', () => {});
+import { GameStore } from '@/world-cup-score-board-lib/GameStore/GameStore';
+
+it('should throw an exception if away team and home team are the same', () => {
+    // prettier-ignore
+    expect(() => new GameStore('Mexico', 'Mexico'))
+        .toThrow('Teams should be different! HomeTeam: [Mexico], AwayTeam: [Mexico]');
+});

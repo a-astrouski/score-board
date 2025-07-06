67d593fdd5c8bb9cd2c11232fddf198f28094a3b
Artem Astrouski <ostrovskiy.a.97@gmail.com>
2025-07-06 21:40:29 +0200

    test: create a list of test cases for the Scoreboard's startGame functionality

---
+++ world-cup-score-board-lib/ScoreBoardStore/ScoreBoardStore.test.ts
@@ -0,0 +1,7 @@
+describe('startGame', () => {
+    it('should create a new game', () => {});
+
+    it('should create a new game with two teams', () => {});
+
+    it('should throw an exception if team already plays in another match', () => {});
+});

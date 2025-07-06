c56e7393cf73817de7e3142b71ebd5442603426b
Artem Astrouski <ostrovskiy.a.97@gmail.com>
2025-07-06 21:52:27 +0200

    feat: implement startGame method for the ScoreBoard service class

--- world-cup-score-board-lib/ScoreBoardStore/ScoreBoardStore.ts
+++ world-cup-score-board-lib/ScoreBoardStore/ScoreBoardStore.ts
@@ -5,9 +5,25 @@ export class ScoreBoardStore {
 
     public constructor() {}
 
-    public startGame() {}
+    public startGame(homeTeam: string, awayTeam: string) {
+        if (this.doesTeamAlreadyPlay(homeTeam)) {
+            throw new Error(`Team [${homeTeam}] already plays in another match!`);
+        } else if (this.doesTeamAlreadyPlay(awayTeam)) {
+            throw new Error(`Team [${awayTeam}] already plays in another match!`);
+        }
+
+        this.games.unshift(new GameStore(homeTeam, awayTeam));
+    }
 
     public finishGame() {}
 
     public getSummaryOfGamesByTotalScore() {}
+
+    private doesTeamAlreadyPlay(team: string): boolean {
+        return (
+            this.games.findIndex(({ homeTeam, awayTeam }) =>
+                [homeTeam.name.toUpperCase(), awayTeam.name.toUpperCase()].includes(team.toUpperCase()),
+            ) !== -1
+        );
+    }
 }

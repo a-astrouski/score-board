5669279c10433ada03d1c347deb4339a3b0c6726
Artem Astrouski <ostrovskiy.a.97@gmail.com>
2025-07-06 21:04:42 +0200

    test: add list of test cases for TeamStore class

---
+++ world-cup-score-board-lib/Team/TeamStore.test.ts
@@ -0,0 +1,13 @@
+it('should throw an exception if team name is not a string', () => {});
+
+it('should throw an exception if team name is a whitespace', () => {});
+
+describe('setScore', () => {
+    it('should set score', () => {});
+
+    it('should throw an exception if new score less than 0', () => {});
+
+    it('should throw an exception if score is not a number', () => {});
+
+    it('should throw an exception if score is higher than 100', () => {});
+});
